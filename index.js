4ca862bbcf935d2a1137e3c46474e31a9689c226
Ryan Clark <ryan.clark.j@gmail.com>
2017-04-24 21:42:21 -0700

    first

---
+++ index.js
@@ -0,0 +1,146 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const Queue = require('p-queue');
+
+/**
+ * Summaries of every stack in a region
+ *
+ * @param {string} region - the AWS region to audit
+ * @param {array} [StackStatusFilter] - an array of stack status values to audit
+ * @returns
+ */
+const listStacks = (region, StackStatusFilter) => {
+  StackStatusFilter = StackStatusFilter || [
+    'CREATE_IN_PROGRESS',
+    'CREATE_FAILED',
+    'CREATE_COMPLETE',
+    'ROLLBACK_IN_PROGRESS',
+    'ROLLBACK_FAILED',
+    'ROLLBACK_COMPLETE',
+    'DELETE_IN_PROGRESS',
+    'DELETE_FAILED',
+    'UPDATE_IN_PROGRESS',
+    'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS',
+    'UPDATE_COMPLETE',
+    'UPDATE_ROLLBACK_IN_PROGRESS',
+    'UPDATE_ROLLBACK_FAILED',
+    'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS',
+    'UPDATE_ROLLBACK_COMPLETE',
+    'REVIEW_IN_PROGRESS'
+  ];
+
+  return new Promise((resolve, reject) => {
+    const cfn = new AWS.CloudFormation({ region });
+    let stacks = [];
+
+    cfn.listStacks({ StackStatusFilter }).eachPage((err, data, done) => {
+      if (err) return reject(err);
+      if (!data) return resolve(stacks);
+      stacks = stacks.concat(data.StackSummaries);
+      done();
+    });
+  });
+};
+
+/**
+ * Get stack summaries and templates matching a set of conditions in a single
+ * region. If no conditions are specified, all stacks will be audited.
+ *
+ * @param {string} region - the AWS region to audit
+ * @param {object} [conditions] - defines filtering criteria
+ * @param {array} [conditions.statuses=any] - an array of stack status values to
+ * @param {function} [conditions.templateFilter=None] - a function that accepts
+ * one template (as a string), and returns true/false (or a Promise that resolves
+ * to true/false) indicating whether or not to return the template in the response
+ * @param {string} [conditions.stage=Processed] - for templates that include
+ * transforms, the stage of the template to return (`Original` or `Processed`).
+ * @returns
+ */
+const getTemplates = (region, conditions) => {
+  conditions = conditions || {};
+  const TemplateStage = conditions.stage || 'Processed';
+
+  const filterer = conditions.templateFilter
+    ? template =>
+        Promise.resolve().then(() => conditions.templateFilter(template))
+    : () => Promise.resolve(true);
+
+  return listStacks(region, conditions.statuses).then(summaries => {
+    const cfn = new AWS.CloudFormation({ region });
+    const queue = new Queue({ concurrency: 1 });
+    const requests = summaries.map(summary =>
+      cfn.getTemplate({
+        StackName: summary.StackName,
+        TemplateStage
+      })
+    );
+
+    const promises = requests.map(req => {
+      const checkTemplate = () =>
+        req
+          .promise()
+          .then(data =>
+            filterer(data.TemplateBody).then(accept => {
+              return accept ? data.TemplateBody : null;
+            })
+          )
+          .catch(err => {
+            if (err.code === 'AccessDenied') return Promise.resolve(null);
+            throw err;
+          });
+
+      return queue.add(checkTemplate);
+    });
+
+    return Promise.all(promises).then(results =>
+      results
+        .map((TemplateBody, i) => ({
+          TemplateBody,
+          Summary: summaries[i],
+          Region: region
+        }))
+        .filter(data => data.TemplateBody)
+    );
+  });
+};
+
+/**
+* Get stack summaries and templates matching a set of conditions in all
+* regions. If no conditions are specified, all stacks will be audited.
+*
+* @param {object} [conditions] - defines filtering criteria
+* @param {array} [conditions.statuses=any] - an array of stack status values to
+* @param {function} [conditions.templateFilter=None] - a function that accepts
+* one template (as a string), and returns true/false (or a Promise that resolves
+* to true/false) indicating whether or not to return the template in the response
+* @param {string} [conditions.stage=Processed] - for templates that include
+* transforms, the stage of the template to return (`Original` or `Processed`).
+ */
+const getWorldWideTemplates = conditions => {
+  const queue = new Queue({ concurrency: 5 });
+  const regions = [
+    'us-east-1',
+    'us-east-2',
+    'us-west-1',
+    'us-west-2',
+    'ca-central-1',
+    'ap-south-1',
+    'ap-northeast-1',
+    'ap-northeast-2',
+    'ap-southeast-1',
+    'ap-southeast-2',
+    'eu-central-1',
+    'eu-west-1',
+    'eu-west-2',
+    'sa-east-1'
+  ];
+
+  return Promise.all(
+    regions.map(region => queue.add(() => getTemplates(region, conditions)))
+  ).then(results =>
+    results.reduce((final, regional) => final.concat(regional), [])
+  );
+};
+
+module.exports = { getTemplates, getWorldWideTemplates };
---
+++ readme.md
@@ -0,0 +1,99 @@
+# cfn-template-audit
+
+Read all the CloudFormation templates in an AWS account
+
+## Usage
+
+#### Which statuses are you interested in?
+
+Specify the stack statuses you are interested in auditing as an array. If you don't care, then the following statuses will be included in your results:
+
+```js
+[
+  'CREATE_IN_PROGRESS',
+  'CREATE_FAILED',
+  'CREATE_COMPLETE',
+  'ROLLBACK_IN_PROGRESS',
+  'ROLLBACK_FAILED',
+  'ROLLBACK_COMPLETE',
+  'DELETE_IN_PROGRESS',
+  'DELETE_FAILED',
+  'UPDATE_IN_PROGRESS',
+  'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS',
+  'UPDATE_COMPLETE',
+  'UPDATE_ROLLBACK_IN_PROGRESS',
+  'UPDATE_ROLLBACK_FAILED',
+  'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS',
+  'UPDATE_ROLLBACK_COMPLETE',
+  'REVIEW_IN_PROGRESS'
+]
+```
+
+#### What are you looking for inside the templates?
+
+Write a function that accepts _a single template as a string_, and then returns `true` or `false` indicating that it should or should not be included in your results. If you want everything, don't bother with this step.
+
+```js
+const templateFilter = template =>
+  JSON.parse(template).Resources.SomeResourceName.Type === 'AWS::SNS::Topic';
+```
+
+If you need to do any asynchronous I/O to determine which templates to include in the results, simply return a Promise that resolves to a a boolean value.
+
+```js
+const templateFilter = template =>
+  new Promise((resolve, reject) =>
+    asynchronousCheck(template, (err, accept) => {
+      if (err) return reject(err);
+      return resolve(accept);
+    })
+  );
+```
+
+#### Roll it up into a script
+
+```js
+const audit = require('@mapbox/cfn-template-audit');
+const conditions = {
+  statuses: ['CREATE_COMPLETE'],
+  templateFilter: template =>
+    JSON.parse(template).Resources.SomeResourceName.Type === 'AWS::SNS::Topic'
+};
+
+audit.getWorldWideTemplates(conditions).then(data =>
+  console.log(
+    data
+      .map(stack => `${stack.Summary.StackName} ${stack.Region}`)
+      .sort((a, b) => {
+        return a > b ? 1 : -1;
+      })
+  );
+);
+```
+
+If you're only interested in a single region, there's a function for that:
+
+```js
+audit.getTemplates('us-east-1', conditions).then(data => ...);
+```
+
+#### What do I get out of this?
+
+You get back an array of objects with the following properties:
+
+```js
+{
+  Summary: {
+    StackId: 'Unique stack identifier',
+    StackName: 'The name associated with the stack',
+    TemplateDescription: 'The template description of the template used to create the stack',
+    CreationTime: 'The time the stack was created',
+    LastUpdatedTime: 'The time the stack was last updated. This field will only be returned if the stack has been updated at least once',
+    DeletionTime: 'The time the stack was deleted',
+    StackStatus: 'The current status of the stack',
+    StackStatusReason: 'Success/Failure message associated with the stack status'
+  },
+  TemplateBody: 'The template itself as a string',
+  Region: 'The AWS region the template is in'
+}
+```
